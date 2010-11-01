6211843482fe4e6293512f9f36c88c5d6e2dd8b1
David Chock <david.chock@gmail.com>
2010-10-31 21:16:56 -0700

    'updatepersonalcalendar'

--- onlinefitness/features/step_definitions/user_steps.rb
+++ onlinefitness/features/step_definitions/user_steps.rb
@@ -1,6 +1,6 @@
 
-Given /^user "([^"]*)" exists$/ do |arg1|
-  @user = User.create(:id => 1, :name => arg1, :birthday => "January 2, 1970", :height => 70, :weight => 150, :gender => "Male")
+Given /^user (\d+) exists$/ do |arg1|
+  @user = User.create(:id => arg1, :name => "John Smith", :birthday => "January 2, 1970", :height => 70, :weight => 150, :gender => "Male")
 end
 
 Then /^"([^"]*)" should be "([^"]*)"$/ do |arg1, arg2|
