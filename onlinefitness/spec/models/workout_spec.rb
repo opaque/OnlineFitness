1239d5bee347b1b194c23668621d206c30a72b86
Cavin Woo <cavin.woo@berkeley.edu>
2010-10-31 19:12:04 -0700

    updated test for workout model

--- onlinefitness/spec/models/workout_spec.rb
+++ onlinefitness/spec/models/workout_spec.rb
@@ -10,17 +10,17 @@ describe Workout do
       :actualreps => 1,
       :actualsets => 1
     }
-	@valid_attr_without_actual_data = {
-	  :date => Time.now,
-	  :exercise => "value for exercise",
-	  :expectedreps => 1,
-	  :expectedsets => 1,
+	@valid_attr_without_actual_data {
+		:date => Time.now,
+		:exercise => "value for exercise",
+		:expectedreps => 1,
+		:expectedsets => 1,
 	}
-	@null_date_attr  ={
-	  :date => nil,
-	  :exercise => "value for exercise",
-	  :expectedreps => 1,
-	  :expectedsets => 1,
+	@null_date_attr {
+		:date => nil,
+		:exercise => "value for exercise",
+		:expectedreps => 1,
+		:expectedsets => 1,
 	}
   end
 
@@ -29,9 +29,10 @@ describe Workout do
 	
   end
   
-  describe "when validating a workout" do
-	describe "when creating a workout" do
+  
 	
+	describe "when creating a workout" do
+
 		it "should create a new movie without actualsets and actualreps" do
 			Workout.create!(@valid_attr_without_actual_data).should be_true
 		end
@@ -40,5 +41,15 @@ describe Workout do
 			Workout.create!(@null_date_attr).should be_true
 		end
 	end
-  end
+	
+	describe "when updating a workout" do
+		
+		it "should update the movie when given actual data" do
+			
+			@workout = Workout.create!(@valid_attr_without_actual_data)
+			@workout.update_attributes(@valid_attributes)
+			(@workout.actualsets == @valid_attributes.actualsets and @workout.actualreps == @valid_attributes.actualreps).should be_true
+		end
+	end
+ 
 end
