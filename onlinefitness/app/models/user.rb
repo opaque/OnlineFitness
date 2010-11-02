b46f677228b1a88a14a22bd9223ac13b36125c35
Edward Wu <eddy771@berkeley.edu>
2010-11-01 22:07:09 -0700

    added presence

--- onlinefitness/app/models/user.rb
+++ onlinefitness/app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
 	has_many :workouts, :dependent => :destroy
+	validates_presence_of :name
+	validates_numericality_of :weight, :allow_nil => true
+	validates_numericality_of :height, :allow_nil => true
 end
